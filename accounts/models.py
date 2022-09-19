c9f5cc049e82874f15bb1c629ad5f6234dab050b
Zakki <zakkinadaf56@gmail.com>
2022-09-19 22:38:29 +0530

    updated

--- accounts/models.py
+++ accounts/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     price=models.FloatField(null=True)
     category=models.CharField(max_length=200,null=True,choices=CATEGORY)
     discription=models.CharField(max_length=200,null=True,blank=True)
-    date_created=models.DateTimeField(auto_now_add=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
     tags=models.ManyToManyField(Tag)
     
     def __str__(self):
@@ -44,7 +44,9 @@ class Order(models.Model):
     )
     customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
     product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
-    date_created=models.DateTimeField(auto_now_add=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=200,null=True,choices=STATUS)
-    
-    
+    note=models.CharField(max_length=1000,null=True)
+
+    def __str__(self):
+        return self.product.name
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,26 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+from .forms import OrderForm,CreateUserForm
+
+
+def registerPage(request):
+    form=CreateUserForm()
+
+    if request.method=="POST":
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context={'form':form}
+    return render(request,'accounts/register.html',context)
+
+def loginPage(request):
+    context={}
+    return render(request,'accounts/login.html',context)
 
 def home(request):
     orders=Order.objects.all()
@@ -21,11 +40,56 @@ def home(request):
 def products(request):
     products=Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
+
 def customer(request,pk_test):
     customer=Customer.objects.get(id=pk_test)
 
     orders=customer.order_set.all()
     order_count=orders.count()
-    context={'customer':customer,'orders':orders,'order_count':order_count}
+
+    myFilter=OrderFilter(request.GET,queryset=orders)
+    orders=myFilter.qs
+
+    context={'customer':customer,'orders':orders,'order_count':order_count,'myFilter':myFilter}
     return render(request,'accounts/customer.html',context)
-# Create your views here.
+
+def createOrder(request,pk):
+    OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer=Customer.objects.get(id=pk)
+    formset=OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #form=OrderForm(initial={'customer':customer})
+    if request.method=='POST':
+        #print('Printing POST',request.POST)
+        #form=OrderForm(request.POST)
+        formset=OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+
+    context={'formset':formset}
+    
+    return render(request, 'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    order=Order.objects.get(id=pk)
+    form=OrderForm(instance=order)
+
+    if request.method=='POST':
+        form=OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    context={'form':form}
+    return render(request, 'accounts/order_form.html',context)
+
+def deleteOrder(request,pk):
+    order=Order.objects.get(id=pk)
+    if request.method=="POST":
+        order.delete()
+        return redirect('/')
+
+    context={'item':order}
+
+    return render(request, 'accounts/delete.html',context)
